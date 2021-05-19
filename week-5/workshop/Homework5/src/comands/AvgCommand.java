38e60be069980821d66c7833591990de71c9f101
Mihail Petrov <mail@mihail-petrov.me>
2021-05-19 14:44:39 +0300

    Add workshop for week 5

---
+++ week-5/workshop/Homework5/src/comands/AvgCommand.java
@@ -0,0 +1,11 @@
+package comands;
+
+public class AvgCommand {
+
+    public static int execute(int[] collection) {
+
+        int length = collection.length;
+        int sum = SumCommand.execute(collection);
+        return  sum / length;
+    }
+}
